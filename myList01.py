ea5dc4b1bf64244d4cde4676f8cfc3b03bf66ab1
bradsmialek <bradsmilek@gmail.com>
2020-07-02 12:06:39 -0400

    adding work to /mycode

---
+++ myList01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+'''
+Document comment
+'''
+
+# inline comment
+def main():
+    movies = []
+    movies.append("back to future")
+
+    movies.append("yes")
+
+    print(movies)
+    print(movies.index("yes"))
+    
+
+if __name__ == "__main__":
+    main()
